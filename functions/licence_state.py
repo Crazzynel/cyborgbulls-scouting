f1281f4fb9534ec7255071735b7b4d4206641425
Nakano San <nakanosan@gmx.fr>
2024-03-30 12:42:30 +0100

    updating code with date

--- functions/licence_state.py
+++ functions/licence_state.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from safer import id1, id2, id3
 
 def userkey():
     # Collecter les informations d'identification
@@ -8,17 +9,16 @@ def userkey():
     currentTime = datetime.now()
 
     # Vérifier la clé de licence
-    if cle_licence == "qdogB1lXTAKans4HLGfX":
+    if cle_licence in (id1, id2, id3):
         hypevaluestate = "Correct Key"
         print(hypevaluestate)
         print("Your ID has been successfully recorded")
-
         # Ouvrir le fichier de journal en mode écriture
         with open("./functions/credentials/user_log.log", "a") as log_file:
             # Écrire les informations d'identification dans le fichier de journal
             log_file.write(f"Nom: {nom},\nPrenom: {prenom}, \nCle de licence: {cle_licence}\nDate et Heure d'ajout d'ID Licence: {currentTime}")
     else:
-        print("Incorrect Key, Veuillez réessayer en redémarrant le programme.")
+        print("Cle incorrecte, vous ne disposez pas de compte CyborgBulls.fr .")
 
 # Appel de la fonction pour collecter les informations d'identification
 # function called on licence_user.cyborgbulls
